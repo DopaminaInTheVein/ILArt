9d0dd5b0a097bec429bd3bcb4d8a2f1ee816f60f
Pedro <pedro.villalonga89@gmail.com>
2016-09-17 12:41:31 +0200

    update dev 17/09

--- bin/data/lua_scripts/ui/title_impl.lua
+++ bin/data/lua_scripts/ui/title_impl.lua
@@ -1,16 +1,22 @@
 
 function OnStart_title( )
+	p:putText("title_text","It Lightens",1.25,0.3,"#FF0000FF",3.5)
 	p:clear_save()
 end
 
 function OnClicked_btn_start( )
-	cam:fade_out(1)
+	--ui_cam:fade_out(1)
 	p:exec_command("playVideoIntro();", 1)
 end
 
+function OnClicked_btn_options( )
+	p:load_entities("options")
+end
+
 function playVideoIntro( )
-	--LoadLevel("level_1")
-	p:play_video_and_do("data\\videos\\intro.avi", "LoadLevel(\"level_1\");")
+	p:removeText("title_text")
+	LoadLevel("level_1")
+	--p:play_video_and_do("data\\videos\\intro.avi", "LoadLevel(\"level_1\");")
 end
 
 function OnClicked_btn_exit( )
