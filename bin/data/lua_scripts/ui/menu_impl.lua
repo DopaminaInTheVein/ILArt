254ba2204f0dced00cf67271233f89378f70ef03
AlexandreAlberti <alberti.bu@gmail.com>
2016-10-24 19:11:56 +0200

    updating version

--- bin/data/lua_scripts/ui/menu_impl.lua
+++ bin/data/lua_scripts/ui/menu_impl.lua
@@ -1,6 +1,13 @@
 p = Public()
 
+function OnCreateGui_btn_resume( )
+	p:pause_game()
+	p:wait_escape("OnClicked_btn_resume();")
+end
+
 function OnClicked_btn_resume( )
+	p:play_sound("event:/OnMenuAccept", 1.0, false)
+	p:wait_escape_cancel()
 	p:set_cursor_enabled(false)
 	destroyMenu()
 	p:resume()
@@ -8,6 +15,8 @@ end
 
 function OnClicked_btn_restart( )
 	-- (Fadeout)
+	p:play_sound("event:/OnMenuAccept", 1.0, false)
+	p:wait_escape_cancel()
 	p:set_cursor_enabled(false)
 	p:exec_command("destroyMenu();", 1)
 	p:exec_command("LoadLevel(g_current_level);", 1.1)
@@ -15,16 +24,23 @@ end
 
 function OnClicked_btn_back_title( )
 	-- (Fadeout)
+	p:play_sound("event:/OnMenuCancel", 1.0, false)
+	p:wait_escape_cancel()
 	p:set_cursor_enabled(false)
 	p:exec_command("destroyMenu();", 1)
 	p:exec_command("LoadLevel(\"level_0\");", 1.1)
 end
 
 function OnClicked_btn_opt_menu( )
+	p:play_sound("event:/OnMenuAccept", 1.0, false)
+	options_from = "pause"
+	p:wait_escape_cancel()
 	p:load_entities("options")
 end
 
 function OnClicked_btn_exit_pause( )
+	p:play_sound("event:/OnMenuCancel", 1.0, false)
+	p:wait_escape_cancel()
 	p:print("exit..")
 	p:exit_game()
 end
